5d3880a158478c2402b9077440a9bf81057cac49
caobaoli <15935498245@163.com>
2019-08-15 00:27:59 +0800

    人人

--- ds_node/ds.sql
+++ ds_node/ds.sql
@@ -5,7 +5,7 @@ USE ds;
 
 CREATE TABLE ds_users(
     uid INT PRIMARY KEY AUTO_INCREMENT,
-    uname VARCHAR(16) UNIQUE,
+    uname VARCHAR(32) UNIQUE,
     upwd VARCHAR(32),
     phone CHAR(11),
     email VARCHAR(60),
@@ -14,4 +14,5 @@ CREATE TABLE ds_users(
 );
 INSERT INTO ds_users VALUES(null,'曹宝利','123456');
 INSERT INTO ds_users (phone,upwd)VALUES('15110800953','123456');
+INSERT INTO ds_users (uname,upwd)VALUES('曹宝利','123456');
 SELECT * FROM ds_users;
